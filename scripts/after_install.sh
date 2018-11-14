4507cd917365b217b7ef78adfbce29508f346e75
Volodymyr Chornonoh <chernonog.vova@gmail.com>
2018-11-14 16:07:48 +0200

    Add hook to remove mute-audio default flag

---
+++ scripts/after_install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Need to comment default audio muting
+
+MUTE_AUDIO="'\-\-mute\-audio'"
+
+comment() {
+    if ! grep -q "//$MUTE_AUDIO" $1; then
+        sed -i -e "s|$MUTE_AUDIO|//$MUTE_AUDIO|g" $1
+    fi
+}
+
+comment node_modules/chrome-launcher/dist/flags.js
+
+comment node_modules/chrome-launcher/dist/src/flags.js
